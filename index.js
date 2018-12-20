7d63d6e224605b7d1e61c52a3aeb4ffdf319286d
Farhan Mahmood <farhanmahmoodny@gmail.com>
2018-12-20 01:27:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value){
   object[key] = value
+  console.lgo(object)
   var newObject = Object.assign({}, object)
   console.log(newObject)
+
 }
