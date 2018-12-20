65b1ea54b488c093c86d22dc89fc12b14a7ce4b2
Farhan Mahmood <farhanmahmoodny@gmail.com>
2018-12-20 01:22:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function updateObjectWithKeyAndValue(object, key, value){
   object[key] = value
   return object
 }
+
+function updaeObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  var newObject = Object.assign({}, object)
+  return newObject
+}
