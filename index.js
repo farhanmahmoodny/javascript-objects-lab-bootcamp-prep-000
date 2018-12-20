e167c7009171d8e9c9975107df0fb4ef97bf9641
Farhan Mahmood <farhanmahmoodny@gmail.com>
2018-12-20 01:47:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value){
   object[key] = value
-  var newObject = Object.assign({}, object)
+  var newObject = Object.assign({}, object, {[key]: value]})
   return newObject
 }
